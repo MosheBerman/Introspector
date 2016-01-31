f512a53f9322bcef4212f1fd83d0c4253434eddf
Moshe Berman <moshberm@gmail.com>
2016-01-31 17:33:51 -0500

    Improved property introspection to be more descriptive and human readable.

--- Introspector/INTIntrospector.m
+++ Introspector/INTIntrospector.m
@@ -80,10 +80,7 @@ NSArray* ClassGetMethods(Class targetClass) {
     for (unsigned int i = 0; i < methodCount; i++) {
         Method method = methods[i];
         
-        printf("\t'%s' has method named '%s' of encoding '%s'\n",
-               class_getName(targetClass),
-               sel_getName(method_getName(method)),
-               method_getTypeEncoding(method));
+        //        printf("\t'%s' has method named '%s' of encoding '%s'\n", class_getName(targetClass), sel_getName(method_getName(method)), method_getTypeEncoding(method));
         
         /**
          *  Or do whatever you need here...
@@ -102,36 +99,10 @@ NSArray* ClassGetMethods(Class targetClass) {
 
 static const char * getPropertyType(objc_property_t property) {
     const char *attributes = property_getAttributes(property);
-    printf("attributes=%s\n", attributes);
-    char buffer[1 + strlen(attributes)];
-    strcpy(buffer, attributes);
-    char *state = buffer, *attribute;
-    while ((attribute = strsep(&state, ",")) != NULL) {
-        if (attribute[0] == 'T' && attribute[1] != '@') {
-            // it's a C primitive type:
-            /*
-             if you want a list of what will be returned for these primitives, search online for
-             "objective-c" "Property Attribute Description Examples"
-             apple docs list plenty of examples of what you get for int "i", long "l", unsigned "I", struct, etc.
-             */
-            return (const char *)[[NSData dataWithBytes:(attribute + 1) length:strlen(attribute) - 1] bytes];
-        }
-        else if (attribute[0] == 'T' && attribute[1] == '@' && strlen(attribute) == 2) {
-            // it's an ObjC id type:
-            return "id";
-        }
-        else if (attribute[0] == 'T' && attribute[1] == '@') {
-            // it's another ObjC object type:
-            
-            const void * bytes = (attribute + 3);
-            NSUInteger length = strlen(attribute);
-            
-            NSData *data = [NSData dataWithBytes:bytes length:length];
-            
-            return (const char *)[data bytes];
-        }
-    }
-    return "";
+    
+    NSString *attributesString = propertyAttributesFromCString(attributes);
+    
+    return [attributesString cStringUsingEncoding:NSUTF8StringEncoding];
 }
 
 
@@ -151,18 +122,161 @@ NSDictionary * classPropsForClass(Class targetClass)
         if(propName) {
             const char *propType = getPropertyType(property);
             NSString *propertyName = [NSString stringWithUTF8String:propName];
-            NSString *propertyType = [NSString stringWithUTF8String:propType];
+            NSString *propertyType = propertyAttributesFromCString(propType);
             if(propertyName && propertyType)
             {
                 [results setObject:propertyType forKey:propertyName];
             }
-
+            
         }
     }
     free(properties);
     
     // returning a copy here to make sure the dictionary is immutable
     return [NSDictionary dictionaryWithDictionary:results];
+}
+
+#pragma mark - Helpers
+
+/**
+ *  Converts an attributes string to a readable description of a property.
+ */
+
+NSString * propertyAttributesFromCString(const char * string)
+{
+    NSMutableString *output = [[NSMutableString alloc] init];
+    
+    NSString *utf8 = [NSString stringWithUTF8String:string];
+    
+    NSArray *components = [utf8 componentsSeparatedByString:@","];
+    NSCharacterSet *atAndQuotesSet = [NSCharacterSet characterSetWithCharactersInString:@"@\""];
+    
+    for (NSString *component in components)
+    {
+        if (output.length > 0)
+        {
+            [output appendString:@", "];
+        }
+        
+        // Known types
+        if (component.length > 3 && [component characterAtIndex:0] == 'T')
+        {
+            NSString *type = [[component componentsSeparatedByCharactersInSet:atAndQuotesSet] componentsJoinedByString:@""]; // Remove @"" wrapper
+            type = [type substringFromIndex:1]; // Remove leading T
+            [output appendString:type];
+        }
+        else if (component.length == 3 && [component characterAtIndex:0] == 'T')
+        {
+            if ([component characterAtIndex:1] == 'b')
+            {
+                [output appendString:@"bitfield (length "];
+                [output appendFormat:@"%c",[component characterAtIndex:2]];
+                [output appendString:@")"];
+            }
+            else if ([component characterAtIndex:1] == '^')
+            {
+                NSString *string = [component substringFromIndex:2];
+                NSString *type = typeForEncodingValue(string);
+                if (type != nil)
+                {
+                    [output appendString:type];
+                    [output appendString:@" *"];
+                }
+                else
+                {
+                    [output appendString:component];
+                }
+            }
+        }
+        else if (typeForEncodingValue(component) != nil)
+        {
+            [output appendString:typeForEncodingValue(component)];
+        }
+        else if (descriptionForEncodingValue(component) != nil)
+        {
+            [output appendString:descriptionForEncodingValue(component)];
+        }
+        // Custom Setter
+        else if ([component characterAtIndex:0] == 'S')
+        {
+            [output appendString:@"setter: "];
+            [output appendString:[component substringFromIndex:1]];
+        }
+        // Custom Getter
+        else if ([component characterAtIndex:0] == 'G')
+        {
+            [output appendString:@"getter: "];
+            [output appendString:[component substringFromIndex:1]];
+        }
+        else if ([component characterAtIndex:0] == 'V' && output.length > 0)
+        {
+            // Don't append backing ivar name.
+        }
+        else
+        {
+            [output appendString:component];
+        }
+        
+    }
+    
+    return output;
+}
+
+NSString *descriptionForEncodingValue(NSString *value) {
+    static NSDictionary *mapping = nil;
+  
+  if (mapping == nil)
+  {
+      mapping = @{
+                  @"R" : @"readonly",
+                  @"C" : @"copy",
+                  @"&" : @"retain",
+                  @"N" : @"nonatomic",
+                  @"D" : @"dynamic",
+                  @"W" : @"weak",
+                  @"P" : @"garbage collected",
+                  };
+  }
+    return mapping[value];
+}
+
+NSString *typeForEncodingValue(NSString *value) {
+    
+    // If not a type, we can't key in.
+    if ([value characterAtIndex:0] != 'T')
+    {
+        return nil;
+    }
+    
+    static NSDictionary *mapping = nil;
+    
+    if (mapping == nil)
+    {
+        mapping = @{
+                    @"B" : @"bool",
+                    @"c" : @"char",
+                    @"d" : @"double",
+                    @"f" : @"float",
+                    @"i" : @"int",
+                    @"l" : @"long",
+                    @"q" : @"long long",
+                    @"s" : @"short",
+                    @"v" : @"void",
+                    @"*" : @"char *",
+                    @"@" : @"id", // ObjC id type
+                    @"#" : @"class",
+                    @":" : @"SEL",
+                    @"C" : @"unsigned char",
+                    @"I" : @"unsigned int",
+                    @"L" : @"unsigned long",
+                    @"Q" : @"unsigned long long"
+                    };
+    }
+    
+    NSString *key = [value substringFromIndex:1]; // Remove leading T.
+    return mapping[key];
+    
+    
 }
 
 @end
