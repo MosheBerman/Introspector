28ddffec059b3772c987e1820b1f7062b10fcb8e
Moshe Berman <moshberm@gmail.com>
2016-01-31 17:48:39 -0500

    Implemented search.

--- Introspector/MasterViewController.m
+++ Introspector/MasterViewController.m
@@ -10,9 +10,13 @@
 #import "DetailViewController.h"
 #import "INTIntrospector.h"
 
-@interface MasterViewController ()
+@interface MasterViewController () <UISearchBarDelegate> {
+    BOOL invalid;
+}
 
 @property NSArray *objects;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
 @end
 
 @implementation MasterViewController
@@ -25,8 +29,10 @@
     
     Class targetClass = NSClassFromString(@"NSObject");
     self.objects = [i subclassesOfClass:targetClass];
-
-    self.title = [NSString stringWithFormat:@"%u Classes", self.objects.count];
+    
+    self.title = [NSString stringWithFormat:@"%lu Classes", (unsigned long)self.searchedClasses.count];
+    
+    invalid = false;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -45,12 +51,10 @@
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        Class object = self.objects[indexPath.row];
+        Class object = self.searchedClasses[indexPath.row];
         
         DetailViewController *dtv = (DetailViewController *)[[segue destinationViewController]topViewController];
         dtv.targetClass = object;
-        
-        
     }
 }
 
@@ -61,13 +65,13 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.objects.count;
+    return self.searchedClasses.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
-    NSDate *object = self.objects[indexPath.row];
+    NSDate *object = self.searchedClasses[indexPath.row];
     cell.textLabel.text = [object description];
     return cell;
 }
@@ -75,6 +79,66 @@
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
+}
+
+#pragma mark - Helpers
+
+- (NSArray *)searchedClasses
+{
+    if (self.searchBar.text.length == 0)
+    {
+        invalid = false;
+        return self.objects;
+    }
+    
+    NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(id  _Nonnull evaluatedObject, NSDictionary<NSString *,id> * _Nullable bindings) {
+        
+        Class c = evaluatedObject;
+        NSString *name = NSStringFromClass(c);
+        
+        NSString *lowercaseSearch = self.searchBar.text.lowercaseString;
+        if ([[name lowercaseString] rangeOfString:lowercaseSearch].location != NSNotFound)
+        {
+            return YES;
+        }
+        else
+        {
+            return NO;
+        }
+        
+    }];
+    
+    static NSArray *results = nil;
+    
+    if (results == nil || invalid == true)
+    {
+        results = [self.objects filteredArrayUsingPredicate:predicate];
+        invalid = false;
+    }
+    
+    return results;
+}
+
+#pragma mark - Search
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    invalid = true;
+    [self.tableView reloadData];
+    self.title = [NSString stringWithFormat:@"%lu Classes", (unsigned long)self.searchedClasses.count];
+}
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar
+{
+    invalid = true;
+    searchBar.text = nil;
+    [self.tableView reloadData];
+    self.title = [NSString stringWithFormat:@"%lu Classes", (unsigned long)self.searchedClasses.count];
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    [searchBar resignFirstResponder];
 }
 
 
